a1e4c548fdf561d2c4bd838fdc10ed8bce8940a8
Artur Manukyan <aptumen@yandex.ru>
2021-03-28 17:38:53 +0300

    les 9

--- bin/brain-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { name } from '../src/cli.js'
-console.log('Welcome to the Brain Games!')
-console.log(`Hello, ${name()}!`)
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
