a193372869e4bc94ceae15ee0aaebb376148d3ec
Artur Manukyan <aptumen@yandex.ru>
2021-03-30 22:10:44 +0300

    исправление ошибки

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,13 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  const userName = name();
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${name}!`);
+  return name;
 };
+
+// export default () => {
+//   console.log('Welcome to the Brain Games!');
+//   const name = readlineSync.question('May I have your name? ');
+//   const userName = name();
+//   console.log(`Hello, ${userName}!`);
+// };
